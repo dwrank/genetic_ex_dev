db2553d88558d6385348ba3277bad22774b87751
Daniel Rank <dwrank@gmail.com>
2022-11-26 17:19:59 -0800

    Initial commmit

---
+++ lib/genetic_ex_dev.ex
@@ -0,0 +1,18 @@
+defmodule GeneticExDev do
+  @moduledoc """
+  Documentation for `GeneticExDev`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GeneticExDev.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/genetic_ex_dev_test.exs
@@ -0,0 +1,8 @@
+defmodule GeneticExDevTest do
+  use ExUnit.Case
+  doctest GeneticExDev
+
+  test "greets the world" do
+    assert GeneticExDev.hello() == :world
+  end
+end
